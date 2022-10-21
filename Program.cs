0aa805a5e49b245d294b585cf83d8ae549f1870e
Tomasz Nasiewicz <114070800+tomasznasiewicz99@users.noreply.github.com>
2022-10-21 14:07:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_9_PierwszyProgramVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Tomasz";
+
+            Console.WriteLine("Witaj " + imie);
+
+        }
+    }
+}
+
